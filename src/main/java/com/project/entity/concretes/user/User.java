99b8d119b0a99dde40f93e8d2e3f005328a46e43
nurullahkaratas@gmail.com <nurullahkaratas@gmail.com>
2023-12-05 22:12:12 +0100

    Entity Classes and Security Layer

--- src/main/java/com/project/entity/concretes/user/User.java
+++ src/main/java/com/project/entity/concretes/user/User.java
@@ -1,11 +1,16 @@
 package com.project.entity.concretes.user;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.entity.concretes.business.LessonProgram;
+import com.project.entity.concretes.business.Meet;
+import com.project.entity.concretes.business.StudentInfo;
 import com.project.entity.enums.Gender;
 import lombok.*;
-
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -66,5 +71,24 @@ public class User {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private UserRole userRole;
 
-    // TODO : StudentInfo - LessonProgram - Meet eklenecek
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.REMOVE)
+    private List<StudentInfo> studentInfos;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_lessonprogram",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "lesson_program_id")
+    )
+    private Set<LessonProgram> lessonProgramList;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "meet_student_table",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "meet_id")
+    )
+    private List<Meet> meetList;
 }
---
+++ src/main/java/com/project/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,22 @@
+package com.project.security.jwt;
+//Genellikle filterlarda loglama islemleri yapilir
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
